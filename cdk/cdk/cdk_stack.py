9f26844944f09262ae96dc29afc7278f30e245be
karthikvetrivel <kvvetrivel@gmail.com>
2024-03-11 22:41:59 -0700

    initial commit 🚀

---
+++ cdk/cdk/cdk_stack.py
@@ -0,0 +1,17 @@
+from constructs import Construct
+from aws_cdk import (
+    Duration,
+    Stack,
+    aws_iam as iam,
+    aws_sqs as sqs,
+    aws_sns as sns,
+    aws_sns_subscriptions as subs,
+    aws_s3 as s3,
+)
+
+
+class CdkStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(self, "MyFirstBucket")   
